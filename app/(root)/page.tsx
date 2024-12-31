dfe6d0912ed602baf57f1e8a0afcf67754f8ea67
antonythomas123 <antonythomas993@gmail.com>
2024-12-31 14:27:49 +0530

    ISR and live data fetch using sanity

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,7 @@
-import { client } from "@/sanity/lib/client";
 import SearchForm from "../components/SearchForm";
 import StartupCard, { StartupTypeCard } from "../components/StartupCard";
 import { STARTUPS_QUERY } from "@/sanity/lib/queries";
+import { sanityFetch, SanityLive } from "@/sanity/lib/live";
 
 export default async function Home({
   searchParams,
@@ -10,7 +10,9 @@ export default async function Home({
 }) {
   const query = (await searchParams).query;
 
-  const posts = await client.fetch(STARTUPS_QUERY);
+  const params = { search: query || null };
+  // const posts = await client.fetch(STARTUPS_QUERY);
+  const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY, params });
 
   return (
     <>
@@ -39,6 +41,7 @@ export default async function Home({
           )}
         </ul>
       </section>
+      <SanityLive />
     </>
   );
 }
