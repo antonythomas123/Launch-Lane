62e02f44ee2e0429c37ad1339b8975abe676046d
antonythomas123 <antonythomas993@gmail.com>
2025-01-02 14:05:58 +0530

    Startup form and validation

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import 'easymde/dist/easymde.min.css'
+import "easymde/dist/easymde.min.css";
+import { Toaster } from "@/components/ui/toaster";
 
 const workSans = localFont({
   src: [
@@ -46,7 +47,7 @@ const workSans = localFont({
       style: "normal",
     },
   ],
-  variable: '--font-work-sans'
+  variable: "--font-work-sans",
 });
 
 export const metadata: Metadata = {
@@ -61,10 +62,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={workSans.variable}
-      >
+      <body className={workSans.variable}>
         {children}
+        <Toaster />
       </body>
     </html>
   );
--- auth.ts
+++ auth.ts
@@ -1,10 +1,77 @@
+import NextAuth from "next-auth";
+import GitHub from "next-auth/providers/github";
+import { client } from "./sanity/lib/client";
+import { AUTHOR_BY_GITHUB_ID_QUERY } from "./sanity/lib/queries";
+import { writeClient } from "./sanity/lib/write-client";
 
-import NextAuth from "next-auth"
-import GitHub from "next-auth/providers/github"
- 
+const secret = process.env.AUTH_SECRET;
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [GitHub],
+  providers: [
+    GitHub({
+      clientId: process.env.AUTH_GITHUB_ID,
+      clientSecret: process.env.AUTH_GITHUB_SECRET,
+    }),
+  ],
   session: { strategy: "jwt" },
   experimental: { enableWebAuthn: true },
-  secret: process.env.AUTH_SECRET
-})
+  secret: secret,
+  callbacks: {
+    async signIn({
+      user: { name, email, image },
+      profile: { id, login, bio },
+    }) {
+      const existingUser = await client
+        .withConfig({ useCdn: false })
+        .fetch(AUTHOR_BY_GITHUB_ID_QUERY, {
+          id,
+        });
+
+      if (!existingUser) {
+        try {
+          await writeClient.create({
+            _type: "author",
+            id,
+            name,
+            username: login,
+            email,
+            image,
+            bio: bio || "",
+          });
+        } catch (e) {
+          console.log(e, "error inside ****************");
+        }
+      }
+
+      return true;
+    },
+    async jwt({ token, account, profile }) {
+      if (account && profile) {
+        const user = await client
+          .withConfig({ useCdn: false })
+          .fetch(AUTHOR_BY_GITHUB_ID_QUERY, {
+            id: profile?.id,
+          });
+
+        token.id = user?._id;
+      }
+
+      return token;
+    },
+    async session({ session, token }) {
+      Object.assign(session, { id: token.id });
+      return session;
+    },
+  },
+  logger: {
+    error(code, ...message) {
+      console.log(code, message);
+    },
+    warn(code, ...message) {
+      console.log(code, message);
+    },
+    debug(code, ...message) {
+      console.log(code, message);
+    },
+  },
+});
