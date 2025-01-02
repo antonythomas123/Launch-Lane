8b9d7be76acb6e57ab0484dbabc340fc0988fd1d
antonythomas123 <antonythomas993@gmail.com>
2025-01-02 14:44:18 +0530

    Startup form submission

--- components/StartupForm.tsx
+++ components/StartupForm.tsx
@@ -9,6 +9,7 @@ import { formSchema } from "@/lib/validation";
 import { z } from "zod";
 import { useToast } from "@/hooks/use-toast";
 import { useRouter } from "next/navigation";
+import { createPitch } from "@/lib/actions";
 
 const StartupForm = () => {
   const [errors, setErrors] = useState<Record<string, string>>({});
@@ -29,19 +30,19 @@ const StartupForm = () => {
 
       await formSchema.parseAsync(formValues);
 
-      console.log(formValues);
+      const result = await createPitch(prevState, formData, pitch);
 
-    //   if (result.status == "SUCCESS") {
-    //     toast({
-    //       title: "Success",
-    //       description: "Your startup pitch has been successfully created",
-    //       variant: "destructive",
-    //     });
+      if (result.status == "SUCCESS") {
+        toast({
+          title: "Success",
+          description: "Your startup pitch has been successfully created",
+          variant: "destructive",
+        });
 
-    //     router.push(`/startup/${result.id}`);
-    //   }
+        router.push(`/startup/${result?._id}`);
+      }
 
-    //   return result;
+      return result;
     } catch (error) {
       if (error instanceof z.ZodError) {
         const fieldErrors = error.flatten().fieldErrors;
